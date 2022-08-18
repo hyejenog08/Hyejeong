dc5339a7919ee94529065b673ddc8e087d9b005c
Hyejeong <100392957+hyejeong08@users.noreply.github.com>
2022-08-18 17:58:10 +0900

    AtCoder practice1 220818

---
+++ AtCoder/src/Practice1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Practice1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+
+        int p = 1;
+        int q = 3;
+
+        System.out.println("p+q = " + (p+q) );
+
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int f = sc.nextInt();
+        int x = sc.nextInt();
+
+        int takahashi = 0;
+        int aoki = 0;
+
+
+
+
+
+
+    }
+
+}
